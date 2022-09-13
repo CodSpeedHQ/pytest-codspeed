4daeb2fc925b85be428bc9ed1aa989b9b4fdd576
Arthur Pastel <arthur.pastel@gmail.com>
2022-09-13 17:22:03 +0200

    Implement pytest marks to run benchmarks on tests

--- src/avalanche_pytest/plugin.py
+++ src/avalanche_pytest/plugin.py
@@ -1,9 +1,12 @@
-from typing import Any, Callable
+from typing import Any, Callable, List
 
 import pytest
 from avalanche.callgrind_wrapper import lib
 
+benchmark_count_key = pytest.StashKey[int]()
 
+
+@pytest.hookimpl()
 def pytest_addoption(parser: "pytest.Parser"):
     group = parser.getgroup("avalanche performance measurement")
     group.addoption(
@@ -20,6 +23,7 @@ def pytest_addoption(parser: "pytest.Parser"):
     )
 
 
+@pytest.hookimpl()
 def pytest_configure(config: "pytest.Config"):
     config.addinivalue_line(
         "markers", "avalanche_benchmark: mark an entire test for avalanche benchmarking"
@@ -29,9 +33,68 @@ def pytest_configure(config: "pytest.Config"):
     )
 
 
+def is_benchmark_enabled(config: "pytest.Config") -> bool:
+    return config.getoption("--benchmark") or config.getoption("--only-benchmark")
+
+
+def should_benchmark_item(item: "pytest.Item") -> bool:
+    return (
+        item.get_closest_marker("avalanche_benchmark") is not None
+        or item.get_closest_marker("abenchmark") is not None
+        or "benchmark" in getattr(item, "fixturenames", [])
+    )
+
+
+@pytest.hookimpl()
+def pytest_sessionstart(session: "pytest.Session"):
+    if is_benchmark_enabled(session.config):
+        session.stash[benchmark_count_key] = 0
+
+
+@pytest.hookimpl(trylast=True)
+def pytest_collection_modifyitems(
+    session: "pytest.Session", config: "pytest.Config", items: "List[pytest.Item]"
+):
+    if config.getoption("--only-benchmark"):
+        deselected = []
+        selected = []
+        for item in items:
+            if should_benchmark_item(item):
+                selected.append(item)
+            else:
+                deselected.append(item)
+        config.hook.pytest_deselected(items=deselected)
+        items[:] = selected
+
+
+@pytest.hookimpl()
+def pytest_runtest_call(item: "pytest.Item"):
+    if not is_benchmark_enabled(item.config) or not should_benchmark_item(item):
+        item.runtest()
+    else:
+        item.session.stash[benchmark_count_key] += 1
+        if "benchmark" in getattr(item, "fixturenames", []):
+            item.runtest()
+        else:
+            lib.zero_stats()
+            lib.start_instrumentation()
+            item.runtest()
+            lib.stop_instrumentation()
+            lib.dump_stats_at(f"{item.nodeid}".encode("ascii"))
+
+
+@pytest.hookimpl()
+def pytest_sessionfinish(session: "pytest.Session", exitstatus):
+    if is_benchmark_enabled(session.config):
+        reporter: pytest.TerminalReporter = session.config.pluginmanager.get_plugin(
+            "terminalreporter"
+        )
+        reporter.write_sep("=", f"{session.stash[benchmark_count_key]} benchmarked")
+
+
 @pytest.fixture(scope="session")
 def _is_benchmark_enabled(request: "pytest.FixtureRequest") -> bool:
-    return request.config.getoption("--benchmark")
+    return is_benchmark_enabled(request.config)
 
 
 @pytest.fixture
--- tests/test_pytest_plugin.py
+++ tests/test_pytest_plugin.py
@@ -24,4 +24,48 @@ def test_plugin_enabled(pytester: pytest.Pytester) -> None:
         """
     )
     result = pytester.runpytest("--benchmark")
-    result.stdout.fnmatch_lines(["*1 passed*"])
+    result.stdout.fnmatch_lines(["*1 benchmarked*", "*1 passed*"])
+
+
+def test_plugin_enabled_nothing_to_benchmark(pytester: pytest.Pytester) -> None:
+    pytester.makepyfile(
+        """
+        def test_some_addition_performance():
+            return 1 + 1
+        """
+    )
+    result = pytester.runpytest("--benchmark")
+    result.stdout.fnmatch_lines(["*0 benchmarked*", "*1 passed*"])
+
+
+def test_plugin_only_benchmark_collection(pytester: pytest.Pytester) -> None:
+    pytester.makepyfile(
+        """
+        import pytest
+
+        @pytest.mark.avalanche_benchmark
+        def test_some_addition_performance():
+            return 1 + 1
+
+        @pytest.mark.abenchmark
+        def test_some_addition_performance_shorthand():
+            return 1 + 1
+
+        def test_some_wrapped_benchmark(benchmark):
+            @benchmark
+            def _():
+                hello = "hello"
+
+        def test_another_useless_thing():
+            assert True
+        """
+    )
+    collection_result = pytester.runpytest("--only-benchmark", "--collect-only")
+    collection_result.stdout.fnmatch_lines(
+        [
+            "*<Function test_some_addition_performance>*",
+            "*<Function test_some_addition_performance_shorthand>*",
+            "*<Function test_some_wrapped_benchmark>*",
+            "*3/4 tests collected (1 deselected)*",
+        ],
+    )
