14446c1fdac64769c3ac3053a0ba498d8b77622d
Ken Odegard <kodegard@anaconda.com>
2024-02-13 15:02:13 -0600

    Replace pytest_runtest_protocol with pytest_pyfunc_call

--- src/pytest_codspeed/plugin.py
+++ src/pytest_codspeed/plugin.py
@@ -1,11 +1,12 @@
 from __future__ import annotations
 
+import functools
 import gc
 import os
 import pkgutil
 import sys
 from dataclasses import dataclass, field
-from typing import TYPE_CHECKING, TypeVar
+from typing import TYPE_CHECKING, ParamSpec, TypeVar
 
 import pytest
 from _pytest.fixtures import FixtureManager
@@ -16,7 +17,7 @@ from . import __version__
 from ._wrapper import get_lib
 
 if TYPE_CHECKING:
-    from typing import Any, Callable
+    from typing import Any, Callable, Iterator
 
     from ._wrapper import LibType
 
@@ -164,94 +165,59 @@ def pytest_collection_modifyitems(
         items[:] = selected
 
 
-def _run_with_instrumentation(
-    lib: LibType,
-    nodeId: str,
-    config: pytest.Config,
-    fn: Callable[..., Any],
-    *args,
-    **kwargs,
-):
-    is_gc_enabled = gc.isenabled()
-    if is_gc_enabled:
-        gc.collect()
-        gc.disable()
-
-    result = None
-
-    def __codspeed_root_frame__():
-        nonlocal result
-        result = fn(*args, **kwargs)
-
-    if SUPPORTS_PERF_TRAMPOLINE:
-        # Warmup CPython performance map cache
-        __codspeed_root_frame__()
-    lib.zero_stats()
-    lib.start_instrumentation()
-    __codspeed_root_frame__()
-    lib.stop_instrumentation()
-    uri = get_git_relative_uri(nodeId, config.rootpath)
-    lib.dump_stats_at(uri.encode("ascii"))
-    if is_gc_enabled:
-        gc.enable()
-
-    return result
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_runtest_protocol(item: pytest.Item, nextitem: pytest.Item | None):
-    plugin = get_plugin(item.config)
-    if not plugin.is_codspeed_enabled or not should_benchmark_item(item):
-        return (
-            None  # Defer to the default test protocol since no benchmarking is needed
-        )
-
-    if has_benchmark_fixture(item):
-        return None  # Instrumentation is handled by the fixture
-
-    plugin.benchmark_count += 1
-    if not plugin.should_measure:
-        return None  # Benchmark counted but will be run in the default protocol
-
-    # Setup phase
-    reports = []
-    ihook = item.ihook
-    ihook.pytest_runtest_logstart(nodeid=item.nodeid, location=item.location)
-    setup_call = pytest.CallInfo.from_call(
-        lambda: ihook.pytest_runtest_setup(item=item, nextitem=nextitem), "setup"
-    )
-    setup_report = ihook.pytest_runtest_makereport(item=item, call=setup_call)
-    ihook.pytest_runtest_logreport(report=setup_report)
-    reports.append(setup_report)
-    # Run phase
-    if setup_report.passed and not item.config.getoption("setuponly"):
-        assert plugin.lib is not None
-        runtest_call = pytest.CallInfo.from_call(
-            lambda: _run_with_instrumentation(
-                plugin.lib,
-                item.nodeid,
-                item.config,
-                lambda: ihook.pytest_runtest_call(item=item),
-            ),
-            "call",
-        )
-        runtest_report = ihook.pytest_runtest_makereport(item=item, call=runtest_call)
-        ihook.pytest_runtest_logreport(report=runtest_report)
-        reports.append(runtest_report)
-
-    # Teardown phase
-    teardown_call = pytest.CallInfo.from_call(
-        lambda: ihook.pytest_runtest_teardown(item=item, nextitem=nextitem), "teardown"
-    )
-    teardown_report = ihook.pytest_runtest_makereport(item=item, call=teardown_call)
-    ihook.pytest_runtest_logreport(report=teardown_report)
-    reports.append(teardown_report)
-    ihook.pytest_runtest_logfinish(nodeid=item.nodeid, location=item.location)
-
-    return reports  # Deny further protocol hooks execution
-
-
 T = TypeVar("T")
+P = ParamSpec("P")
+
+
+def wrap_pyfunc_with_instrumentation(
+    lib: LibType,
+    nodeid: str,
+    config: pytest.Config,
+    fn: Callable[P, T],
+) -> Callable[P, T]:
+    @functools.wraps(fn)
+    def wrapper(*args: P.args, **kwargs: P.kwargs) -> T:
+        is_gc_enabled = gc.isenabled()
+        if is_gc_enabled:
+            gc.collect()
+            gc.disable()
+        try:
+            if SUPPORTS_PERF_TRAMPOLINE:
+                # Warmup CPython performance map cache
+                fn(*args, **kwargs)
+
+            lib.zero_stats()
+            lib.start_instrumentation()
+            try:
+                return fn(*args, **kwargs)
+            finally:
+                lib.stop_instrumentation()
+                uri = get_git_relative_uri(nodeid, config.rootpath)
+                lib.dump_stats_at(uri.encode("ascii"))
+        finally:
+            if is_gc_enabled:
+                gc.enable()
+
+    return wrapper
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_pyfunc_call(pyfuncitem: pytest.Function) -> Iterator[None]:
+    plugin = get_plugin(pyfuncitem.config)
+    if (
+        plugin.is_codspeed_enabled
+        and should_benchmark_item(pyfuncitem)
+        and not has_benchmark_fixture(pyfuncitem)
+    ):
+        plugin.benchmark_count += 1
+        if plugin.lib is not None and plugin.should_measure:
+            pyfuncitem.obj = wrap_pyfunc_with_instrumentation(
+                plugin.lib,
+                pyfuncitem.nodeid,
+                pyfuncitem.config,
+                pyfuncitem.obj,
+            )
+    yield
 
 
 class BenchmarkFixture:
@@ -266,11 +232,17 @@ class BenchmarkFixture:
         config = self._request.config
         plugin = get_plugin(config)
         plugin.benchmark_count += 1
-        if plugin.is_codspeed_enabled and plugin.should_measure:
-            assert plugin.lib is not None
-            return _run_with_instrumentation(
-                plugin.lib, self._request.node.nodeid, config, func, *args, **kwargs
-            )
+        if (
+            plugin.is_codspeed_enabled
+            and plugin.lib is not None
+            and plugin.should_measure
+        ):
+            return wrap_pyfunc_with_instrumentation(
+                plugin.lib,
+                self._request.node.nodeid,
+                config,
+                func,
+            )(*args, **kwargs)
         else:
             return func(*args, **kwargs)
 
