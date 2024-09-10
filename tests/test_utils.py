83812c7c3fca43f3e06cbbb2157c6820702ab945
Arthur Pastel <arthur.pastel@gmail.com>
2024-09-10 12:51:51 +0200

    chore: fix utils test using a fake git repo

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,28 +1,32 @@
+import tempfile
+from contextlib import contextmanager
 from pathlib import Path
-from unittest.mock import patch
 
 from pytest_codspeed.utils import get_git_relative_path, get_git_relative_uri
 
 
+@contextmanager
+def TemporaryGitRepo():
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        (Path(tmpdirname) / ".git").mkdir(parents=True)
+        yield tmpdirname
+
+
 def test_get_git_relative_path_found():
-    with patch.object(
-        Path, "exists", lambda self: str(self) == "/home/user/gitrepo/.git"
-    ):
-        path = Path("/home/user/gitrepo/folder/nested_folder")
+    with TemporaryGitRepo() as tmp_repo:
+        path = Path(tmp_repo) / "folder/nested_folder"
         assert get_git_relative_path(path) == Path("folder/nested_folder")
 
 
 def test_get_git_relative_path_not_found():
-    with patch.object(Path, "exists", lambda self: False):
-        path = Path("/home/user/gitrepo/folder")
+    with tempfile.TemporaryDirectory() as tmp_dir:
+        path = Path(tmp_dir) / "folder"
         assert get_git_relative_path(path) == path
 
 
 def test_get_git_relative_uri():
-    with patch.object(
-        Path, "exists", lambda self: str(self) == "/home/user/gitrepo/.git"
-    ):
-        pytest_rootdir = Path("/home/user/gitrepo/pytest_root")
+    with TemporaryGitRepo() as tmp_repo:
+        pytest_rootdir = Path(tmp_repo) / "pytest_root"
         uri = "testing/test_excinfo.py::TestFormattedExcinfo::test_fn"
         assert (
             get_git_relative_uri(uri, pytest_rootdir)
