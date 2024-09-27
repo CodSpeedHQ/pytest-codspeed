d1c3db809e4b70eb2bc7ee04f931ae9b73240df3
Arthur Pastel <arthur.pastel@gmail.com>
2024-09-27 16:06:47 +0200

    Release v3.0.0b4 🚀

--- src/pytest_codspeed/__init__.py
+++ src/pytest_codspeed/__init__.py
@@ -1,6 +1,6 @@
-__version__ = "3.0.0b3"
+__version__ = "3.0.0b4"
 # We also have the semver version since __version__ is not semver compliant
-__semver_version__ = "3.0.0-b3"
+__semver_version__ = "3.0.0-b4"
 
 from .plugin import BenchmarkFixture
 
