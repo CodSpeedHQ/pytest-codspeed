b03d88af5427e58f078f21416152264223235aa5
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-05 17:40:47 +0100

    feat: check buildability and fallback when build doesn't work

--- setup.py
+++ setup.py
@@ -1,19 +1,41 @@
+import importlib.util
 import os
 import platform
+from pathlib import Path
 
 from setuptools import setup
+
+build_path = (
+    Path(__file__).parent / "src/pytest_codspeed/instruments/valgrind/_wrapper/build.py"
+)
+
+spec = importlib.util.spec_from_file_location("build", build_path)
+assert spec is not None, "The spec should be initialized"
+build = importlib.util.module_from_spec(spec)
+assert spec.loader is not None, "The loader should be initialized"
+spec.loader.exec_module(build)
 
 system = platform.system()
 current_arch = platform.machine()
 
-IS_EXTENSION_REQUIRED = (
-    system == "Linux"
-    and current_arch
-    in [
-        "x86_64",
-        "arm64",
-    ]
-    or os.environ.get("PYTEST_CODSPEED_FORCE_EXTENSION") is not None
+IS_EXTENSION_BUILDABLE = system == "Linux" and current_arch in [
+    "x86_64",
+    "arm64",
+]
+
+IS_EXTENSION_REQUIRED = os.environ.get("PYTEST_CODSPEED_FORCE_EXTENSION") is not None
+if IS_EXTENSION_REQUIRED and not IS_EXTENSION_BUILDABLE:
+    raise ValueError(
+        "The extension is required but the current platform is not supported"
+    )
+
+
+ffi_extension = build.ffibuilder.distutils_extension()
+ffi_extension.optional = not IS_EXTENSION_REQUIRED
+
+print(
+    "CodSpeed native extension is "
+    + ("required" if IS_EXTENSION_REQUIRED else "optional")
 )
 
 setup(
@@ -23,9 +45,5 @@ setup(
             "instruments/valgrind/_wrapper/*.c",
         ]
     },
-    cffi_modules=[
-        "src/pytest_codspeed/instruments/valgrind/_wrapper/build.py:ffibuilder"
-    ]
-    if IS_EXTENSION_REQUIRED
-    else [],
+    ext_modules=[ffi_extension] if IS_EXTENSION_BUILDABLE else [],
 )
