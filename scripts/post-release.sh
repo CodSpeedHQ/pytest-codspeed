e7ef8537bca9ea5d72fbcc2003536721b4b8faef
Arthur Pastel <arthur.pastel@gmail.com>
2024-12-06 00:43:59 +0100

    chore: fix release script

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 set -e
 
-VERSION=v$BUMPVER_NEW_VERSION
+VERSION=$BUMPVER_NEW_VERSION
 
+# We handle tagging here since bumpver doesn't allow custom
+# tagnames and we want a v prefix
 git tag v$VERSION -m "Release v$VERSION 🚀"
 git push --follow-tags
