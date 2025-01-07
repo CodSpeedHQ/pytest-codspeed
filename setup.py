64900a815c61176123f07d9dab971502a449675b
Arthur Pastel <arthur.pastel@gmail.com>
2025-01-07 16:30:41 +0100

    ci: add a py3-none-any fallback wheel

--- setup.py
+++ setup.py
@@ -28,11 +28,18 @@ IS_EXTENSION_BUILDABLE = system == "Linux" and current_arch in [
 IS_EXTENSION_REQUIRED = (
     os.environ.get("PYTEST_CODSPEED_FORCE_EXTENSION_BUILD") is not None
 )
+
+SKIP_EXTENSION_BUILD = (
+    os.environ.get("PYTEST_CODSPEED_SKIP_EXTENSION_BUILD") is not None
+)
+
+if SKIP_EXTENSION_BUILD and IS_EXTENSION_REQUIRED:
+    raise ValueError("Extension build required but the build requires to skip it")
+
 if IS_EXTENSION_REQUIRED and not IS_EXTENSION_BUILDABLE:
     raise ValueError(
         "The extension is required but the current platform is not supported"
     )
-
 
 ffi_extension = build.ffibuilder.distutils_extension()
 ffi_extension.optional = not IS_EXTENSION_REQUIRED
@@ -43,11 +50,14 @@ print(
 )
 
 setup(
-    package_data={
-        "pytest_codspeed": [
-            "instruments/valgrind/_wrapper/*.h",
-            "instruments/valgrind/_wrapper/*.c",
-        ]
+    exclude_package_data={
+        "pytest_codspeed.instruments.valgrind._wrapper": [
+            "*.c",
+            "*.h",
+            "build.py",
+        ],
     },
-    ext_modules=[ffi_extension] if IS_EXTENSION_BUILDABLE else [],
+    ext_modules=(
+        [ffi_extension] if IS_EXTENSION_BUILDABLE and not SKIP_EXTENSION_BUILD else []
+    ),
 )
